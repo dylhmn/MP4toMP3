1733680f19960b71ca90df4bec24cd8e9a484fb7
dylhmn <dylharman.18@gmail.com>
2025-02-28 22:08:48 +0000

    UI implementation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace mp4to3
 {
-    partial class Form1
+    partial class frmConvert
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblRibbon = new System.Windows.Forms.Label();
+            this.picMin = new System.Windows.Forms.PictureBox();
+            this.picExit = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picExit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblRibbon
+            // 
+            this.lblRibbon.AutoSize = true;
+            this.lblRibbon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.lblRibbon.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblRibbon.Location = new System.Drawing.Point(-3, 0);
+            this.lblRibbon.Name = "lblRibbon";
+            this.lblRibbon.Size = new System.Drawing.Size(590, 24);
+            this.lblRibbon.TabIndex = 0;
+            this.lblRibbon.Text = "                                                                                 " +
+    "                                   ";
+            // 
+            // picMin
+            // 
+            this.picMin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.picMin.Image = global::mp4to3.Properties.Resources.min_asset;
+            this.picMin.Location = new System.Drawing.Point(517, 3);
+            this.picMin.Name = "picMin";
+            this.picMin.Size = new System.Drawing.Size(20, 20);
+            this.picMin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMin.TabIndex = 1;
+            this.picMin.TabStop = false;
+            this.picMin.Click += new System.EventHandler(this.picMin_Click);
+            // 
+            // picExit
+            // 
+            this.picExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.picExit.Image = global::mp4to3.Properties.Resources.exit_asset;
+            this.picExit.Location = new System.Drawing.Point(554, 3);
+            this.picExit.Name = "picExit";
+            this.picExit.Size = new System.Drawing.Size(20, 20);
+            this.picExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picExit.TabIndex = 2;
+            this.picExit.TabStop = false;
+            this.picExit.Click += new System.EventHandler(this.picExit_Click);
+            // 
+            // frmConvert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.ControlBox = false;
+            this.Controls.Add(this.picExit);
+            this.Controls.Add(this.picMin);
+            this.Controls.Add(this.lblRibbon);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmConvert";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.picMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picExit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblRibbon;
+        private System.Windows.Forms.PictureBox picMin;
+        private System.Windows.Forms.PictureBox picExit;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,62 @@ using System.Windows.Forms;
 
 namespace mp4to3
 {
-    public partial class Form1 : Form
+    public partial class frmConvert : Form
     {
-        public Form1()
+        public frmConvert()
         {
             InitializeComponent();
+
+            // Assigning the established mouse events to move the form
+            this.lblRibbon.MouseDown += new MouseEventHandler(this.lblRibbon_MouseDown);
+            this.lblRibbon.MouseMove += new MouseEventHandler(this.lblRibbon_MouseMove);
+            this.lblRibbon.MouseUp += new MouseEventHandler(this.lblRibbon_MouseUp);
         }
+        // VARIABLES
+        bool drag;
+        Point displacement;
+
+        //
+        // SECTION 1 - UI
+        //
+        private void lblRibbon_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                drag = true;
+                displacement = new Point(e.X, e.Y);
+            } // MOUSE EVENT - Upon a left click, record the position and set drag = true
+            else if (e.Button == MouseButtons.Right)
+            {
+                this.CenterToScreen();
+            }// MOUSE EVENT - The form is centered on a right click
+        }
+        private void lblRibbon_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag) // (When the mouse is down)
+            {
+                this.Location = new Point(
+                    this.Location.X + e.X - displacement.X,
+                    this.Location.Y + e.Y - displacement.Y);
+            } // The calculations for the form to move with the mouse
+        }
+        private void lblRibbon_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+        } // Drag release
+
+        private void picExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        } // Exit button safely closes the program
+
+        private void picMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        //
+        // SECTION 2 -
+        //
     }
 }
